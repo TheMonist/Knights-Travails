0f819a3ac81820c4e19456bd3a6d995127006d2b
TheMonist <rogerrosena@gmail.com>
2023-08-08 12:53:05 -0400

    Writing all possible knight moves function

---
+++ knightsTravails.js
@@ -0,0 +1,21 @@
+// Creates chess game board
+const createBoard = () => {
+    // Insert function here
+}
+
+// Creates knight
+
+// All possible moves a knight could make
+const possibleMoves = () => {
+    [
+        [-2,-1], [-2,1],
+        [-1, 2], [-1, -2],
+        [2, -1], [2, 1],
+        [1, 2], [1, -2] 
+    ]
+}
+
+// Creates knight move
+const knightMove = (start, end) => {
+    // Insert function here
+}
