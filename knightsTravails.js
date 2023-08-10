191618c6a7c4e19c0495ade849b34f1f7afd9e41
TheMonist <rogerrosena@gmail.com>
2023-08-09 20:20:11 -0400

    working on knightMove function

--- knightsTravails.js
+++ knightsTravails.js
@@ -21,8 +21,6 @@ const createBoard = (size = 8) => {
     }
 }
 
-// Creates knight
-
 // All possible moves a knight could make
 const validMove = (row, col) => {
     return row >= 0 && row < size && col >= 0 && col < size;
@@ -43,22 +41,42 @@ const getPossiblePath = (row, col) => {
 
 // Creates knight move
 const knightMove = (start, end) => {
-    
-    /* BFS -> You need something like this
-    const levelOrder = (callback) => {
-        if (root === null) return [];
-        const queue = [root];
-        const result = []
-        while (queue.length > 0) {
-            const node = queue.shift();
-            if (node.leftChild != null) queue.push(node.leftChild);
-            if (node.rightChild != null) queue.push(node.rightChild);
-            if (callback) callback(node);
-            else result.push(node.data);
+    const board = createBoard();
+
+    if ((!board.validMove(start[0], start[1])) || (!board.validMove(end[0], end[1]))) {
+        throw new error("Not a valid coordinate");
+    }
+
+    // Find Path
+    let queue = [start];
+    while(queue.length !== 0) {
+        const currentCoord = queue.shift();
+        if (currentCoord[0] === end[0] && currentCoord[1] === end[1]) {
+            break;
         }
 
-        return result;
+        const nextCoord = getPossiblePath(possibleMoves(currentCoord[0], currentCoord[1]));
+        nextCoord.forEach((coord) => {
+            if (board.visited[coord[0][coord[1]]]) {
+                return;
+            }
+            queue.push(coord);
+            board.visited[coord[0]][coord[1]] = true;
+            board.lastCoord[coord[0]][coord[1]] = [currentCoord[0], currentCoord[1]];
+        });
     }
-    */
 
+    // Backtrace Path
+    const path = [];
+    let currentCoord = end; 
+    while (currentCoord[0] !== start[0] && currentCoord[1] !== start[1]) {
+        path.unshift(currentCoord);
+        currentCoord = board.lastCoord[currentCoord[0]][currentCoord[1]];
+    }
+    path.unshift(start);
+
+    // Output
+    console.log(`You made it in ${path.length} moves! Here is your path: `);
+    path.forEach(coord => console(coord));
+    return path;
 }
